6e564ce841ab0f7ac6b56fe22b5568859d76d177
bram aribowo <donyaribowo@gmail.com>
2018-10-24 11:41:48 +0700

    success sending message to cloudamq

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
 var express = require('express'),
 	methodOverride = require('method-override');
-    bodyParser = require('body-parser');
+  bodyParser = require('body-parser');
 	restful = require('node-restful'),
 	mongoose =  restful.mongoose;
 
-var generate_mongo_url = function(obj){
-  obj.hostname = (obj.hostname || 'localhost');
-  obj.port = (obj.port || 27017);
-  obj.db = (obj.db || 'test');
-
-  if(obj.username && obj.password){
-    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-  }
-  else{
-    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-  }
-}
+var amqp = require('amqplib/callback_api');
 
 if(process.env.VCAP_SERVICES){
   var env = JSON.parse(process.env.VCAP_SERVICES);
-  var mongourl = env.mlab[0].credentials.uri
+  var mongourl = env.mlab[0].credentials.uri;
+  var rabbiturl = env.cloudamqp[0].credentials.uri;
 }
 else{
-  var mongo = {
-    "hostname":"localhost",
-    "port":27017,
-    "username":"",
-    "password":"", 
-    "name":"",
-    "db":"restful-prototype"
-  }
+  var mongourl = "mongodb://localhost/restful-prototype";
+  var rabbiturl = "amqp://localhost";
+}
+var sendMessage = function(msg){
+    amqp.connect(rabbiturl, function(err, conn) {
+      conn.createChannel(function(err, ch) {
+        var q = 'audit';
 
-  var mongourl = generate_mongo_url(mongo);
+        ch.assertQueue(q, {durable: false});
+        ch.sendToQueue(q, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+      });
+      setTimeout(function() { conn.close(); }, 500);
+    });
 }
 
 var app = express();
@@ -39,12 +33,9 @@ var port = (process.env.PORT  || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 app.use(methodOverride());
 // parse application/json
-app.use(bodyParser.json());                        
-
+app.use(bodyParser.json()); 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
 // mongoose.connect('mongodb://localhost/restful-prototype');
 mongoose.connect(mongourl);
 
@@ -55,7 +46,26 @@ var ProductSchema = mongoose.Schema({
 });
 var Products = restful.model('products', ProductSchema);
 Products.methods(['get', 'put','post','delete']);
+
+Products.after('post', function(req, res, next) {
+  console.log("posting message");
+  sendMessage("inserting product: " + req.body.sku);
+  next(); // Don't forget to call next!
+});
+
+Products.after('put', function(req, res, next) {
+  console.log("updating message");
+  sendMessage("updating product: " + req.body.sku);
+  next(); // Don't forget to call next!
+});
+
+Products.after('delete', function(req, res, next) {
+  console.log("deleting message");
+  sendMessage("deleting product: " + req.body.sku);
+  next(); // Don't forget to call next!
+});
 Products.register(app, '/api/products');
 
 app.listen(port, host);
+// sendMessage("test message product: ");
 console.log("Server is on air, port 3000");
